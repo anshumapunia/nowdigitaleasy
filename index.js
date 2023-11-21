f5675bddcbc99b65ce765274377c9a04a6480fc6
anshumapunia123 <112813318+anshumapunia123@users.noreply.github.com>
2023-11-21 20:59:13 +0530

    nowdigitaleasy

--- index.js
+++ index.js
@@ -22,12 +22,6 @@ mongoose.connection.on('error', (err) => {
 
 // Use the userRoutes
 
-app.get("/api",(req,res)=>{
-    res.status(201).json({
-        status:true,
-        msg:"welcome to homepage"
-    })
-})
 app.use('/api', userRoutes);
 
 // Server Setup
