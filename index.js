fc81ac30cd84a0c1e06eafc1f7539fb0817e1f78
anshumapunia123 <112813318+anshumapunia123@users.noreply.github.com>
2023-11-21 21:14:07 +0530

    nowdigitaleasy

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ const userRoutes = require('./routes/userRoutes');
 const app = express();
 app.use(bodyParser.json());
 
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        status:true,
+        msg:"Server is Live"
+    })
+})
+
 // MongoDB Connection
-mongoose.connect('mongodb+srv://anshumapunia0:anshumapunia0@cluster0.1af6tyi.mongodb.net/nowdigitaleasy?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect('mongodb+srv://anshumapunia0:anshumapunia0@cluster0.1af6tyi.mongodb.net/nowdigitaleasy?retryWrites=true&w=majority');
 
 mongoose.connection.on('connected', () => {
   console.log('Connected to MongoDB');
